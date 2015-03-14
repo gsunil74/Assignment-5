419474770c40646b95e1fe4af7c6244b9eca94b4
Sidhartha Gopinath <sidharthagopinath@outlook.com>
2015-03-14 19:50:03 -0400

    Assignment 5

--- src/java/prod/products.java
+++ src/java/prod/products.java
@@ -8,7 +8,9 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.json.Json;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -44,28 +46,42 @@ public class products {
     }
 
     private String getResults(String query, String... params) {
-        JSONArray jArray = new JSONArray();
+        Boolean Result = false;
+        JsonArrayBuilder pList = Json.createArrayBuilder();
         StringBuilder sb = new StringBuilder();
         try (Connection cn = credentials.getConnection()) {
             PreparedStatement pstmt = cn.prepareStatement(query);
             for (int i = 1; i <= params.length; i++) {
                 pstmt.setString(i, params[i - 1]);
+                Result = true;
             }
             ResultSet rs = pstmt.executeQuery();
 
-            while (rs.next()) {
-                JSONObject json = new JSONObject();
-                json.put("PRODUCT_ID", rs.getInt("PRODUCT_ID"));
-                json.put("PRODUCT_NAME", rs.getString("PRODUCT_NAME"));
-                json.put("PRODUCT_DESCRIPTION", rs.getString("PRODUCT_DESCRIPTION"));
-                json.put("QUANTITY", rs.getInt("QUANTITY"));
-                jArray.add(json);
+            if (Result == false) {
+                while (rs.next()) {
+                    JsonObjectBuilder productBuilder = Json.createObjectBuilder();
+                    productBuilder.add("productId", rs.getInt("product_id"))
+                            .add("name", rs.getString("product_name"))
+                            .add("description", rs.getString("product_description"))
+                            .add("quantity", rs.getInt("quantity"));
+                    pList.add(productBuilder);
+                }
+            } else {
+                while (rs.next()) {
+                    JsonObject jsonObt = Json.createObjectBuilder()
+                            .add("productId", rs.getInt("product_id"))
+                            .add("name", rs.getString("product_name"))
+                            .add("description", rs.getString("product_description"))
+                            .add("quantity", rs.getInt("quantity"))
+                            .build();
+                    return jsonObt.toString();
+                }
             }
 
         } catch (SQLException ex) {
             Logger.getLogger(products.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return jArray.toJSONString();
+        return pList.build().toString();
     }
 
     private int doUpdate(String query, String... params) {
